d5cf6db7df79fe75087e0743bc53b99c6109d8e8
Mateus Alves Costa <mateuscosta3497@gmail.com>
2026-04-25 10:21:35 -0300

    refactor: TaskRequestDTO para record

--- src/main/java/com/unisales/api_mensageria/dto/TaskRequestDTO.java
+++ src/main/java/com/unisales/api_mensageria/dto/TaskRequestDTO.java
@@ -3,19 +3,14 @@ package com.unisales.api_mensageria.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Map;
 
-@Getter
-@Setter
-public class TaskRequestDTO {
+public record TaskRequestDTO(
+        @NotBlank
+        @JsonProperty("queue_name")
+        String queueName,
 
-    @NotBlank
-    @JsonProperty("queue_name")
-    private String queueName;
-
-    @NotNull
-    private Map<String, Object> payload;
-}
+        @NotNull
+        Map<String, Object> payload
+) {}
